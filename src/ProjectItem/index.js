4317be61c6fdf5a6eb2169e8ee0ed6cf7ad16212
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-04-04 21:30:05 -0700

    adds some generic styling

--- src/ProjectItem/index.js
+++ src/ProjectItem/index.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ProjectItem extends Component {
-   constructor(props) {
-      super(props);
-   }
+  constructor(props) {
+    super(props);
+  }
 
-   render() {
-      const project = this.props.project;
-      return (
-         <div className="project">
-            <a href={project.liveURL} target="_blank">
-               <img
-                  src={project.featuredImage}
-                  alt={project.introDescription}
-               />
-               <h4>{project.name}</h4>
-               <h6>{project.built_with}</h6>
-            </a>
-         </div>
-      );
-   }
+  render() {
+    const project = this.props.project;
+    return (
+      <div className="project">
+        <a href={project.liveURL} target="_blank">
+          <img src={project.featuredImage} alt={project.introDescription} />
+          <h4>{project.name}</h4>
+          <h6>Built With: {project.built_with}</h6>
+        </a>
+      </div>
+    );
+  }
 }
 
 export default ProjectItem;
