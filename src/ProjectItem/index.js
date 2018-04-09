91706e894cf51efb3e928f13da5576f370eb814b
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-04-09 15:35:46 -0700

    adds some projects that I have been working on lately

--- src/ProjectItem/index.js
+++ src/ProjectItem/index.js
@@ -7,21 +7,24 @@ class ProjectItem extends Component {
 
    render() {
       const project = this.props.project;
-      return (
-         <div className="project">
-            <a href={project.liveURL} target="_blank">
-               <img
-                  src={project.featuredImage}
-                  alt={project.introDescription}
-               />
-               <div className="content">
-                  <h4>{project.name}</h4>
-                  <h5>{project.type_of_development}</h5>
-                  <h6>Built With: {project.built_with}</h6>
-               </div>
-            </a>
-         </div>
-      );
+      if (project.status === 'published') {
+         return (
+            <div className="project">
+               <a href={project.liveURL} target="_blank">
+                  <img
+                     src={project.featuredImage}
+                     alt={project.introDescription}
+                  />
+                  <div className="content">
+                     <h4>{project.name}</h4>
+                     <h5>{project.type_of_development}</h5>
+                     <h6>Built With: {project.built_with}</h6>
+                  </div>
+               </a>
+            </div>
+         );
+      }
+      return false;
    }
 }
 
