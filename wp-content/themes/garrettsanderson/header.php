daf4e6c500403c971afa4e0facd891ce28b23871
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-08-02 10:59:57 -0700

    adds image folders

--- wp-content/themes/garrettsanderson/header.php
+++ wp-content/themes/garrettsanderson/header.php
@@ -76,73 +76,7 @@ if ($iphone || $android || $palmpre || $ipod || $berry || $ipad == true) { ?>
 
 <a id="uplink" class="twhite pad1 scrollto" href="#top"><i class="fa fa-chevron-up" aria-hidden="true"></i></a>
 
-<!--[if lt IE 7]>
-    <div class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</div>
-<![endif]-->
-
 <header>
 
-  <div id="headertop" class="fullbg fixedbg bgprimary">
-  <div class="wrapper clearfix">
-
-    <div id="topsnipe" class="cycle-slideshow"  data-cycle-timeout="6000" data-cycle-speed="1000" data-cycle-slides="> h2" >
-      <?php if( have_rows('top_snipe','options') ): ?>
-          <?php while( have_rows('top_snipe','options') ): the_row();
-            $snipeframe = get_sub_field('snipe_frame');
-            $snipelink = get_sub_field('snipe_link');
-          ?>
-          <h2 class="open-sans-semi-bold">
-          <?php if( $snipelink ): ?><a  class="tcenter" href="<?php echo $snipelink; ?>"><?php endif; ?>
-          <?php echo $snipeframe; ?>
-          <?php if( $snipelink ): ?></a><?php endif; ?>
-          </h2>
-          <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
-
-    <div id="topsocial" class="clearfix">
-      <h2 id="topnumber" class="left tcenter open-sans-semi-bold"><?php echo the_field('phone','option'); ?></h2>
-      <ul class="right">
-          <?php get_template_part( 'content-social' ); ?>
-      </ul>
-    </div>
-
-    </div>
-  </div>
-
-  <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container'=>false, 'menu_class'=> 'main-menu' ) ); ?>
 
 </header>
-
-<div id="slider" class="cycle-slideshow" data-cycle-fx="scrollHorz" data-cycle-speed="500" data-cycle-timeout="5000" data-cycle-slides="> .slide">
-
-  <?php if (is_page('home')) { $images = get_field('home_slides'); ?> // Home Page
-
-    <?php foreach( $images as $image ): ?>
-    <div class="slide cover parallax-left responsive-background-image"><?php acf_responsive_image($image); ?></div>
-    <?php endforeach; ?>
-
-    <h1><?php echo the_field('slide_title'); ?></h1>
-
-  <?php } else if (is_home() || is_single() || is_category()) { $images = get_field('top_slides', 64); ?> // News section
-
-    <?php foreach( $images as $image ): ?>
-      <div class="slide cover parallax-left responsive-background-image"><?php acf_responsive_image($image); ?></div>
-    <?php endforeach; ?>
-
-  <?php } else if (is_page() && $post->post_parent > 0) { $images = get_field('top_slides', $post->post_parent); ?> // Child Page
-
-  <?php foreach( $images as $image ): ?>
-    <div class="slide cover parallax-left responsive-background-image"><?php acf_responsive_image($image); ?></div>
-  <?php endforeach; ?>
-
-<?php } else { $images = get_field('top_slides');  ?> // All Others
-
-    <?php foreach( $images as $image ): ?>
-      <div class="slide cover parallax-left responsive-background-image"><?php acf_responsive_image($image); ?></div>
-    <?php endforeach; ?>
-  <?php } ?>
-
-  <a id="downarrow" class="scrollto" href="#homeintro"><img src="<?php echo get_template_directory_uri(); ?>/images/home/arrow-down.svg"></a>
-
-</div>
