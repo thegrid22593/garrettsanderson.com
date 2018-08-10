9d2e76dc19102ab40141f7a184de8672101831a1
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-08-10 11:35:14 -0700

    cleans up the project video player js file

--- wp-content/themes/garrettsanderson/includes/js/GS.Project.js
+++ wp-content/themes/garrettsanderson/includes/js/GS.Project.js
@@ -3,71 +3,56 @@
 var GS = GS || {};
 
 GS.Project = (function () {
+  var $projectVideoPlayer = $ ('.project-video-player');
 
-    var $projectVideoPlayer = $('.project-video-player');
+  var init = function () {
+    getVideos ();
+  };
 
-    var init = function () {
-        getVideos();
+  var getVideos = function () {
+    $.ajax ({
+      type: 'GET',
+      dataType: 'json',
+      url: 'https://api.vimeo.com/me/videos?access_token=e2251ba8ce08942fec8e4afcb7c8f066',
+      success: videos => {
+        videos = videos.data;
+        initPlayer ();
+      },
+    });
+  };
+
+  var initPlayer = function () {
+    var vimeoVideoId = $projectVideoPlayer.data ('vimeo-video-id');
+
+    const options = {
+      id: vimeoVideoId,
+      loop: false,
     };
 
-    var getVideos = function () {
-        $.ajax({
-            type: "GET",
-            dataType: "json",
-            url: "https://api.vimeo.com/me/videos?access_token=e2251ba8ce08942fec8e4afcb7c8f066",
-            success: videos => {
-                videos = videos.data;
-                console.log(videos);
+    var player = new Vimeo.Player ($projectVideoPlayer, options);
 
-                console.log($projectVideoPlayer);
-
-                initPlayer();
-
-            }
-        });
-    }
-
-    var initPlayer = function () {
-
-        var vimeoVideoId = $projectVideoPlayer.data('vimeo-video-id');
-
-        const options = {
-            id: vimeoVideoId,
-            loop: false
-        };
-
-        var player = new Vimeo.Player($projectVideoPlayer, options);
-
+    player
+      .setColor ('#00aaff')
+      .then (function () {
         player
-            .setColor("#00aaff")
-            .then(function () {
-                player
-                    .loadVideo(videoID)
-                    .then(function () {
-                        player.play();
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    });
-            })
-            .catch(error => {
-                console.log(error);
-            });
+          .loadVideo (videoID)
+          .then (function () {
+            player.play ();
+            player.setVolume (0);
+          })
+          .catch (error => {
+            console.log (error);
+          });
+      })
+      .catch (error => {
+        console.log (error);
+      });
+  };
 
-        player.setVolume(1);
+  return {
+    init: init,
+    initPlayer: initPlayer,
+  };
+}) ();
 
-        $(".project-overlay, .video-close").on("click", function () {
-            player.pause();
-            $vimeoPlayerContainer.removeClass("active");
-            $overlay.removeClass("active");
-            $closeIcon.removeClass("active");
-        });
-    };
-
-    return {
-        init: init,
-        initPlayer: initPlayer,
-    };
-})();
-
-GS.Project.init();
+GS.Project.init ();
